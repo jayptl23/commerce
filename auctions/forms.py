fdc4527423a8b4f0279bf4a63a18e78a1904a9af
jayptl23 <>
2023-10-31 00:47:35 -0400

    create ModelForm and html template for create listing

---
+++ auctions/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Listing, Category
+
+
+class ListingForm(forms.ModelForm):
+    class Meta():
+        model = Listing
+        fields = ["title", "description", "image_url", "category"]
+
+        labels = {"image_url": "Image URL"}
+
+    # Define a custom widget for the category field
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        empty_label=None
+    )
