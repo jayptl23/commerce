830229373e0acf6c1548a300939dbce4b05b78ee
jayptl23 <>
2023-10-31 00:46:33 -0400

    create Category and Listing models; register them to admin

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=75, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url = models.URLField()
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="listings")
