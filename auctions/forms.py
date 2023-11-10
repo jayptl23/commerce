25638ceffbf99742bf7ab07c0d3b1af8b347b5d5
jayptl23 <>
2023-11-09 20:42:22 -0500

    create listing form styling

--- auctions/forms.py
+++ auctions/forms.py
@@ -5,14 +5,21 @@ from .models import Listing, Category, Bid
 class ListingForm(forms.ModelForm):
     class Meta():
         model = Listing
-        fields = ["title", "description",
-                  "image_url", "category"]
+        fields = ["title", "description", "image_url", "category", "price"]
         labels = {"image_url": "Image URL"}
 
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": "3"}),
+            "image_url": forms.URLInput(attrs={"class": "form-control"}),
+            "price": forms.NumberInput(attrs={"class": "form-control"})
+        }
+    
     # Define a custom widget for the category field
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(),
-        empty_label=None
+        empty_label=None,
+        widget=forms.Select(attrs={"class": "custom-select"})
     )
 
 class BidForm(forms.ModelForm):
